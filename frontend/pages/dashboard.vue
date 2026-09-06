65a34aadef039268aacb81b4a166fa4ed74e027c
takiguchii <alexandre.pupim360@gmail.com>
2026-09-06 00:15:56 -0300

    feat: definicao da dashboard como rota padrao e ajustes de redirecionamento

---
+++ frontend/pages/dashboard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: [
+    function () {
+      return navigateTo("/", { replace: true });
+    }
+  ]
+});
+</script>
+
+<template>
+  <div />
+</template>
